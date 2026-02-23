805e14b4c94347da329b62450160ad3ae7d9ca72
Milan Ricoul <mricoul@beapi.fr>
2026-02-23 15:00:19 +0000

    Release version 1.0.5

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'beapi/blockparty-icons',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '309f33fb23bca4c4bdd3c790a3c8e0bc12ed27b8',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'beapi/blockparty-icons' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '309f33fb23bca4c4bdd3c790a3c8e0bc12ed27b8',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
